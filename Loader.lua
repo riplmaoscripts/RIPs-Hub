ae5add42ec210c17b91c89b3a8a448033672a02b
riplmaoscripts <139930302+riplmaoscripts@users.noreply.github.com>
2023-09-10 20:44:47 -0400

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,9 @@
+local id = game.PlaceId
+
+local supported = { 14236123211, 13127800756 }
+
+if table.find(supported, id) then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/riplmaoscripts/RIPs-Hub/main/'..id))()
+else
+  game.Players.LocalPlayer:Kick("💀 RIP's Hub | Invalid Game")
+end
